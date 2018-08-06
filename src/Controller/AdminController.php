f91cc7a2d670f877dc072754b5d4950ffea57159
Alejandro Castro <alejandrocastronantes@gmail.com>
2018-08-07 01:57:11 +0200

    [UPDATE] USERS Y LAYOUT DEL LOGIN

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -15,4 +15,23 @@ class AdminController extends EasyAdminController
     protected $request;
     /** @var EntityManager The Doctrine entity manager for the current entity */
     protected $em;
+
+    
+   public function createNewUserEntity()
+   {
+       return $this->get('fos_user.user_manager')->createUser();
+   }
+ 
+   public function prePersistUserEntity($user)
+   {
+       $this->get('fos_user.user_manager')->updateUser($user, false);
+   }
+ 
+   public function preUpdateUserEntity($user)
+ 
+   {
+       $this->get('fos_user.user_manager')->updateUser($user, false);
+   }
+ 
+ 
 }
